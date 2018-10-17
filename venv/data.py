afc971cdc4377311f114210cf6372db6ec4312ae
Hari Narayanan <hnarayanan5@gmail.com>
2018-10-17 21:11:42 +0530

    lol

---
+++ venv/data.py
@@ -0,0 +1,38 @@
+def  Articles():
+    articles=[
+        {
+
+
+
+            'id':1,
+            'Name':'Hari',
+            'About Him':'lllllkjalkjalkjalkalksjdlhjaskjlkjl jk liusa bdsiaudiv jhkjhbjh',
+            'Age': 21,
+            'BirthMonth':'July'
+
+        },
+
+        {
+
+            'id':2,
+            'Name':'Giri',
+            'About Him':'lllllkjalkjalkjalkalksjdlhjaskjlkjl jk liusa bdsiaudiv jhkjhbjh',
+            'Age': 27,
+            'BirthMonth':'September'
+        },
+        {
+
+
+            'id':3,
+            'Name':'Assembly',
+            'About Him':'lllllkjalkjalkjalkalksjdlhjaskjlkjl jk liusa bdsiaudiv jhkjhbjh',
+            'Age': 24,
+            'BirthMonth':'January'
+        }
+
+
+
+
+    ]
+
+    return articles
